9e4bdf5fb08cb9dacf949ae0dc8f677ac463a7bd
Blastboom Strice <65232133+BlastboomStrice@users.noreply.github.com>
2024-10-25 15:16:58 +0300

    Update 3rdPartyAutoUpdater.sh
    V1.1.0 
    more apps :)
    more standard-adhering code (?)
    changed contains($biaryType) in jq with | grep -E "binaryType"

--- 3rdPartyAutoUpdater.sh
+++ 3rdPartyAutoUpdater.sh
@@ -30,16 +30,20 @@ declare -i programIndex=0	#the index of each object in the .json array
 initialize_log(){
 	timeStamp=$(date +'%F_%H:%M:%S')	#current timestamp in yyyy-mm-dd_hh:mm:ss format
 	echo "Current timestamp: $timeStamp"
-	cat <<< $(jq --arg timeStamp "$timeStamp" '(.[] | select (.programName == "null")).timestamp |= $timeStamp' 3rdPartyAutoUpdaterConf.json) > 3rdPartyAutoUpdaterConf.json #updates timestamp in Conf file
+	cat <<< "$(jq --arg timeStamp "$timeStamp" '(.[] | select (.programName == "null")).timestamp |= $timeStamp' 3rdPartyAutoUpdaterConf.json)" > 3rdPartyAutoUpdaterConf.json #updates timestamp in Conf file
 }
 
 #FETCHERS (some repos needs custom fetchers because they don't follow the standard)
 github_fetcher(){
-	curl -s https://api.github.com/repos/$programID/releases/latest | jq --arg binaryType "$binaryType" -r '(.assets[] | select(.name | contains($binaryType))).browser_download_url' | read newUrl	#finds the url for the file and writes it to $newUrl
+
+	curl -s "https://api.github.com/repos/$programID/releases/latest" | jq --arg binaryType "$binaryType" -r '(.assets[] | select(.name)).browser_download_url' | grep -E "$binaryType" | read -r newUrl	#finds the url for the file and writes it to $newUrl
+
+	#the contains() doesn't use regex, so I had to give it up, as one app needed to grep x64.*deb
+	#curl -s https://api.github.com/repos/$programID/releases/latest | jq --arg binaryType "$binaryType" -r '(.assets[] | select(.name | contains($binaryType))).browser_download_url' | read -r newUrl	#finds the url for the file and writes it to $newUrl
 }
 
 gitlab_openrgb_fetcher(){
-	curl -sL https://gitlab.com/api/v4/projects/$programID/releases/permalink/latest | jq --arg binaryType "$binaryType" -r '.description' | grep -oE "$binaryType.*\)" | cut -f2 -d"(" | cut -f1 -d")" | read newUrl
+	curl -sL "https://gitlab.com/api/v4/projects/$programID/releases/permalink/latest" | jq --arg binaryType "$binaryType" -r '.description' | grep -oE "$binaryType.*\)" | cut -f2 -d"(" | cut -f1 -d")" | read -r newUrl
 	#We want the link between here:
 	#[Debian Bookworm amd64](https://openrgb.org/releases/release_0.9/openrgb_0.9_amd64_bookworm_b5f46e3.deb)\n\n[Debian Bookworm i386]
 	#Prints text without escape sequences, grabs the line with the link I want, and cuts anything after "(" and before ")"
@@ -50,7 +54,7 @@ gitlab_openrgb_fetcher(){
 }
 
 gitlab_veloren_fetcher(){
-	curl -sL https://gitlab.com/api/v4/projects/$programID/releases/permalink/latest | jq --arg binaryType "$binaryType" -r '(.assets.links[] | select(.name==$binaryType)).direct_asset_url' | read newUrl
+	curl -sL "https://gitlab.com/api/v4/projects/$programID/releases/permalink/latest" | jq --arg binaryType "$binaryType" -r '(.assets.links[] | select(.name==$binaryType)).direct_asset_url' | read  -r newUrl
 }
 
 #FETCHER Picks fetcher
@@ -66,14 +70,90 @@ fetcher(){
 		gitlab_openrgb_fetcher
 		;;
 	*)
-		echo "This type of fetcher does not exist. Implement it or check for typo in 5th arg in version_check_and_update() of $2"
+		echo "This type of fetcher does not exist. Implement it or check for typo in fetcherType item in json object of $programName"
 		exit	#exits program, there is a failure
 		;;
 	esac
 
-	cat <<< $(jq --arg programName "$programName" --arg newUrl "$newUrl" -r '(.[] | select (.programName==$programName)).url |= $newUrl' 3rdPartyAutoUpdaterConf.json) > 3rdPartyAutoUpdaterConf.json	#updates url of program in Conf file
+	cat <<< "$(jq --arg programName "$programName" --arg newUrl "$newUrl" -r '(.[] | select (.programName==$programName)).url |= $newUrl' 3rdPartyAutoUpdaterConf.json)" > 3rdPartyAutoUpdaterConf.json	#updates url of program in Conf file
 
 	echo "THIS IS THE URL: $newUrl"	#testing stuff here
+}
+
+#DOWNLOADER
+downloader(){
+	#create temp dir
+	mkdir -p "$dpath" #creates temporary directory to extract files
+	#download
+	wget -N "$newUrl" -P "$dpath" #donwloads the file
+	fileName=$(basename "$newUrl") #creates the filename by keeping only anything after the last "/" in the url
+	cat <<< "$(jq --arg programName "$programName" --arg fileName "$fileName" -r '(.[] | select (.programName==$programName)).fileName |= $fileName' 3rdPartyAutoUpdaterConf.json)" > 3rdPartyAutoUpdaterConf.json	#updates filename of program in Conf file
+	echo "THIS IS THE FILENAME: $fileName" #testing stuff here
+}
+
+#NON-DEB-INSTALLER #might need different name if we start fetching AppImages
+non_deb_installer(){
+	echo "Clearing $programName Dir"
+	sudo rm -rf "/opt/$programName/"
+	#cleans old installation files
+
+	if [ "$fileExt" != "null" ]; then	#as long as the program is not an uncompressed file, do
+		echo "Deleting downloaded file"
+		rm -f "$dpath$fileName"
+		#deletes downloaded file after extraction
+	fi
+
+	echo "Creating $programName Dir"
+	sudo mkdir "/opt/$programName/"
+	#creates new programDir
+
+	echo "Moving extracted files to $programName Dir"
+	sudo mv "$dpath"* "/opt/$programName/"
+	#moves files to /opt/programDir/
+
+	if [ "$programExe" != "null" ]; then	#as long as the program has an executable file, do
+		echo "Removing old $programName symlink"
+		sudo rm -f "/usr/bin/$programName"	#removes old symlink
+		echo "Creating new $programName symlink"
+		sudo ln -s "/opt/$programName/$programExe" "/usr/bin/$programName"	#custom for each program
+		#creates symlink to bin
+	fi
+}
+
+#EXTRACTING-INSTALLING
+installer(){
+	echo "Installing..."
+
+	#obsolete:
+	#fileExt=$(echo "$fileName" | grep -o ".\{4\}$")	#grabs the extension of the file (we mostly care if it is .deb or .zip)
+	#echo "$fileExt file"
+
+	case $fileExt in
+	deb)
+		echo "Performing apt install"
+		sudo apt-get -y install "$dpath$fileName" #-y autoinstall dependencies
+		#echo "Deleting downloaded file"
+		#rm -f "$dpath$fileName"	#deletes downloaded file after extraction
+		;;
+	zip)
+		unzip "$dpath$fileName" -d "$dpath"
+		non_deb_installer
+		;;
+	tar)	#tar.* files
+		tar -xvf "$dpath$fileName" -C "$dpath"	#extracts tar
+		non_deb_installer
+		;;
+	null)
+		non_deb_installer
+		;;
+	*)
+		echo "This type of file extention does not exist. Implement it or check for typo in fileExt item in json object of $programName"
+		exit	#exits program, there is a failure
+		;;
+	esac
+
+	echo "Cleaning $dpath Dir contents"
+	rm -rf "$dpath/*" #removes dir where packages where downloaded
 }
 
 #VERSION-CHECK
@@ -88,63 +168,6 @@ else
 fi
 }
 
-#DOWNLOADER
-downloader(){
-	#create temp dir
-	mkdir -p "$dpath" #creates temporary directory to extract files
-	#download
-	wget -N $newUrl -P "$dpath" #donwloads the file
-	fileName=$(basename "$newUrl") #creates the filename by keeping only anything after the last "/" in the url
-	cat <<< $(jq --arg programName "$programName" --arg fileName "$fileName" -r '(.[] | select (.programName==$programName)).fileName |= $fileName' 3rdPartyAutoUpdaterConf.json) > 3rdPartyAutoUpdaterConf.json	#updates filename of program in Conf file
-	echo "THIS IS THE FILENAME: $fileName" #testing stuff here
-}
-
-#EXTRACTING-INSTALLING
-installer(){
-	echo "Installing..."
-	
-	fileExt=$(echo "$fileName" | grep -o ".\{4\}$")	#grabs the extension of the file (we mostly care if it is .deb or .zip)
-	echo "$fileExt file"
-
-	if [ "$fileExt" = ".deb" ]; then
-		echo "Performing apt install"
-		sudo apt install "$dpath$fileName"
-		echo "Deleting downloaded file"
-		rm -f "$dpath$fileName"	#deletes downloaded file after extraction
-	else	#we break the if, because the steps after instruction are the same for the rest of the conditions
-		if [ "$fileExt" = ".zip" ]; then
-			unzip "$dpath$fileName" -d "$dpath"
-		else	#it's gonna be a tar.* file
-			tar -xvf "$dpath$fileName" -C "$dpath"	#extracts tar
-		fi
-		
-		echo "Clearing $programName Dir"
-		sudo rm -rf "/opt/$programName/"
-		#cleans old installation files
-		
-		echo "Deleting downloaded file"
-		rm -f "$dpath$fileName"
-		#deletes downloaded file after extraction
-		
-		echo "Creating $programName Dir"
-		sudo mkdir "/opt/$programName/"
-		#creates new programDir
-		
-		echo "Moving extracted files to $programName Dir"
-		sudo mv "$dpath"* "/opt/$programName/"
-		#moves files to /opt/programDir/
-
-		if [ "$programExe" != "null" ]; then	#as long as the program has an executable file, do
-			echo "Removing old $programName symlink"
-			sudo rm -f /usr/bin/$programName	#removes old symlink
-			echo "Creating new $programName symlink"
-			sudo ln -s /opt/$programName/$programExe /usr/bin/$programName	#custom for each program
-			#creates symlink to bin
-		fi
-	fi
-}
-
-
 #START
 initialize_log
 
@@ -157,12 +180,13 @@ cat 3rdPartyAutoUpdaterConf.json | jq -c '.[]' | cat -n | while read -r i obj; d
     echo "$programIndex"
 
 	#WRITES all the necessary variables
-	programName=$(jq --argjson programIndex $programIndex -r '.[$programIndex].programName' 3rdPartyAutoUpdaterConf.json)	#gets the programName from .json and removes the "". The argjson parses the programIndex as an int insteaf of string
-	programID=$(jq --argjson programIndex $programIndex -r '.[$programIndex].programID' 3rdPartyAutoUpdaterConf.json)	#gets the programID from .json and removes the "". The argjson parses the programIndex as an int insteaf of string
-	url=$(jq --argjson programIndex $programIndex -r '.[$programIndex].url' 3rdPartyAutoUpdaterConf.json)	#gets the url from .json and removes the "". The argjson parses the programIndex as an int insteaf of string
-	fetcherType=$(jq --argjson programIndex $programIndex -r '.[$programIndex].fetcherType' 3rdPartyAutoUpdaterConf.json)	#gets the fetcherType from .json and removes the "". The argjson parses the programIndex as an int insteaf of string
-	binaryType=$(jq --argjson programIndex $programIndex -r '.[$programIndex].binaryType' 3rdPartyAutoUpdaterConf.json)	#gets the binaryType from .json and removes the "". The argjson parses the programIndex as an int insteaf of string
-	programExe=$(jq --argjson programIndex $programIndex -r '.[$programIndex].programExe' 3rdPartyAutoUpdaterConf.json)	#gets the  (relative path to)  programExe from .json and removes the "". The argjson parses the programIndex as an int insteaf of string
+	programName=$(jq --argjson programIndex $programIndex -r '.[$programIndex].programName' 3rdPartyAutoUpdaterConf.json)	#gets the programName from .json and removes the "". The argjson parses the programIndex as an int instead of a string
+	programID=$(jq --argjson programIndex $programIndex -r '.[$programIndex].programID' 3rdPartyAutoUpdaterConf.json)	#gets the programID from .json and removes the "". The argjson parses the programIndex as an int instead of a string
+	url=$(jq --argjson programIndex $programIndex -r '.[$programIndex].url' 3rdPartyAutoUpdaterConf.json)	#gets the url from .json and removes the "". The argjson parses the programIndex as an int instead of a string
+	fetcherType=$(jq --argjson programIndex $programIndex -r '.[$programIndex].fetcherType' 3rdPartyAutoUpdaterConf.json)	#gets the fetcherType from .json and removes the "". The argjson parses the programIndex as an int instead of a string
+	binaryType=$(jq --argjson programIndex $programIndex -r '.[$programIndex].binaryType' 3rdPartyAutoUpdaterConf.json)	#gets the binaryType from .json and removes the "". The argjson parses the programIndex as an int instead of a string
+	fileExt=$(jq --argjson programIndex $programIndex -r '.[$programIndex].fileExt' 3rdPartyAutoUpdaterConf.json)	#gets the  fileExt from .json and removes the "". The argjson parses the programIndex as an int instead of a string
+	programExe=$(jq --argjson programIndex $programIndex -r '.[$programIndex].programExe' 3rdPartyAutoUpdaterConf.json)	#gets the  (relative path to)  programExe from .json and removes the "". The argjson parses the programIndex as an int instead of a string
 
 	#RUNS the functions
 	version_check_and_update
